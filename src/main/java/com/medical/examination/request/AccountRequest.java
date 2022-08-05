f4efc3278763a356a39da4f56d35d6dda564a312
minhvunamdinh <vudmhe140017@fpt.edu.vn>
2022-08-05 21:39:13 +0700

    Update validate register

--- src/main/java/com/medical/examination/request/AccountRequest.java
+++ src/main/java/com/medical/examination/request/AccountRequest.java
@@ -16,7 +16,7 @@ import lombok.Data;
 @Data
 public class AccountRequest {
 	@NotNull(message = "Thông tin bắt buộc")
-    @Size(min = 6, max = 50, message = "Độ dài phải từ 6 đến 255 ký tự")
+    @Size(min = 6, max = 50, message = "Độ dài phải từ 6 đến 50 ký tự")
 	private String username;
 	@NotNull(message = "Thông tin bắt buộc")
     //@Size(min = 6, max = 20, message = "Độ dài phải từ 6 đến 20 ký tự")
@@ -27,7 +27,7 @@ public class AccountRequest {
 	@NotNull(message = "Thông tin bắt buộc")
 	private Long isActive;
 	@NotNull(message = "Thông tin bắt buộc")
-    @Size(min = 6, max = 50, message = "Độ dài phải từ 6 đến 255 ký tự")
+    @Size(min = 6, max = 50, message = "Độ dài phải từ 6 đến 50 ký tự")
 	private String fullname;
 	@NotNull(message = "Thông tin bắt buộc")
     @Size(min = 6, max = 255, message = "Độ dài phải từ 6 đến 255 ký tự")
